1cf3ecc8ff8ad348be22915865f24774b33d5ffe
Gustavo Coronel <gcoronelc@gmail.com>
2020-02-29 12:10:11 -0500

    Clase 03

--- CLASE03/subconsultas.sql
+++ CLASE03/subconsultas.sql
@@ -1,3 +1,49 @@
+-- Ejercicio
+/*
+Mostrar los empleados que su salario es
+menor que el salario promedio.
+*/
+
+select avg(salary) promedio
+from HR.employees;
+
+-- Usando subconsultas
+
+select 
+  e.*,
+  round((select avg(salary) from HR.employees),2) promedio
+from HR.employees e
+where salary < (select avg(salary) from HR.employees);
+
+
+-- Usando with
+
+with
+v1 as (select round(avg(salary),2) promedio from HR.employees)
+select e.*,(select promedio from v1) promedio
+from HR.employees e
+where salary < (select promedio from v1);
+
+
+/*
+SE NECESITA SABER LA CANTIDAD DE EMPLEADOS Y 
+EL IMPORTE DE LA PLANILLA POR DEPARTAMENTO.
+*/
+
+with 
+v1 as (
+  SELECT 
+    deptno,
+    COUNT(1) EMPLEADOS,
+    SUM(sal) PLANILLA,
+    SUM(sal + NVL(e.comm,0)) PLANILLA2
+  FROM SCOTT.emp e
+  GROUP BY deptno
+)
+select d.deptno, d.dname, v1.planilla, v1.planilla2
+from scott.dept d 
+join v1 on d.deptno = v1.deptno;
+
 
 
 
